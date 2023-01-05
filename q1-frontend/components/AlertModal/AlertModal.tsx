a32cf264dc0e8d7aee1282019bd6a040703f7673
Akhilesh Sureshkumar <Akhilesh.Asthagiri@gmail.com>
2023-01-05 23:25:16 +1100

    Q1 Frontend completed

--- q1-frontend/components/AlertModal/AlertModal.tsx
+++ q1-frontend/components/AlertModal/AlertModal.tsx
@@ -1,22 +1,33 @@
 import { SetStateAction, Dispatch, FormEvent } from "react";
-import { TableContents } from "../Table/Table";
+import Table, { TableContents } from "../Table/Table";
 
 interface AlertModalProps {
-  useContents: Dispatch<SetStateAction<TableContents>>,
+    useContents: Dispatch<SetStateAction<TableContents>>,
+    contents: TableContents
 }
 
-export default function AlertModal({useContents}: AlertModalProps) {
-  function onSubmitEvent(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    // hint: the alert given is at (e.target as any).elements[0].value - ignore typescript being annoying
-    console.log((e.target as any)[0].value);
-  }
-  
-  return (
-    <form data-testid='form' onSubmit={onSubmitEvent}>
-      <label> Add new alert: </label>
-      <input type='text' id='alert' name='alert' />
-      <button type='submit'> Add </button>
-    </form>
-  )
+export default function AlertModal({ useContents, contents }: AlertModalProps) {
+    function onSubmitEvent(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        // hint: the alert given is at (e.target as any).elements[0].value - ignore typescript being annoying
+        var newRows = contents.rowContents
+        newRows.push({
+            alert: (e.target as any).elements[0].value,
+            status: '',
+            updates: []
+        })
+        useContents({
+            columnTitles: ['Alert', 'Status', 'Updates'],
+            rowContents: newRows
+        })
+
+    }
+
+    return (
+        <form data-testid='form' onSubmit={onSubmitEvent}>
+            <label> Add new alert: </label>
+            <input type='text' id='alert' name='alert' />
+            <button type='submit'> Add </button>
+        </form>
+    )
 }
--- q1-frontend/components/Table/Table.tsx
+++ q1-frontend/components/Table/Table.tsx
@@ -3,75 +3,83 @@
 import { useState } from "react";
 import AlertModal from "../AlertModal";
 import styles from "./Table.module.css";
+import { SetStateAction, Dispatch, FormEvent } from "react";
 
 // !!!!!!!!!!!!!!!!!!!!
 // TODO is at line 68 !
 // !!!!!!!!!!!!!!!!!!!!
 
 interface AlertUpdate {
-  date: string,
-  update: string
+    date: string,
+    update: string
 }
 
 interface Alert {
-  alert: string,
-  status: string,
-  updates: AlertUpdate[]
+    alert: string,
+    status: string,
+    updates: AlertUpdate[]
 }
 
 export interface TableContents {
-  columnTitles: string[],
-  rowContents: Alert[]
+    columnTitles: string[],
+    rowContents: Alert[]
 }
 
 export default function Table() {
-  const [contents, useContents] = useState<TableContents>({
-    columnTitles: ['Alert', 'Status', 'Updates'],
-    rowContents: [
-      {
-        alert: 'food',
-        status: 'good!',
-        updates: []
-      },
-      {
-        alert: 'water',
-        status: 'low',
-        updates: [{ update: 'dropped to 10% below normal', date: '11/11/2022' }]
-      },
-      {
-        alert: 'shelter',
-        status: 'terrible :(',
-        updates: [{ update: 'slept on cold ground', date: '11/11/2022' }, { update: 'slept on hard concrete', date: '13/11/2022' }]
-      },
-      {
-        alert: 'Done!',
-        status: '<YOUR NAME>',
-        updates: []
-      }
-    ]
-  });
+    const [contents, useContents] = useState<TableContents>({
+        columnTitles: ['Alert', 'Status', 'Updates'],
+        rowContents: [
+            {
+                alert: 'food',
+                status: 'good!',
+                updates: []
+            },
+            {
+                alert: 'water',
+                status: 'low',
+                updates: [{ update: 'dropped to 10% below normal', date: '11/11/2022' }]
+            },
+            {
+                alert: 'shelter',
+                status: 'terrible :(',
+                updates: [{ update: 'slept on cold ground', date: '11/11/2022' }, { update: 'slept on hard concrete', date: '13/11/2022' }]
+            },
+            {
+                alert: 'Done!',
+                status: '<akhil-sureshkumar>',
+                updates: []
+            }
+        ]
+    });
 
-  return (
-    <>
-      <AlertModal useContents={useContents} />
-      <div className={styles.myTable}>
-        <div className={styles.row}>
-          {contents.columnTitles.map((item) => <div className={styles.item} key={item}>{item}</div>)}
-        </div>
-        {contents.rowContents.map((content) => (
-          <div data-testid='row' className={styles.row}>
-            <div className={styles.item}>
-              {content.alert}
+    return (
+        <>
+            <AlertModal useContents={useContents} contents={contents} />
+            <div className={styles.myTable}>
+                <div className={styles.row}>
+                    {contents.columnTitles.map((item) => <div className={styles.item} key={item}>{item}</div>)}
+                </div>
+                {contents.rowContents.map((content) => (
+                    <div data-testid='row' className={styles.row}>
+                        <div className={styles.item}>
+                            {content.alert}
+                        </div>
+                        <div className={styles.item}>
+                            {content.status}
+                        </div>
+                        <div className={styles.item}>
+                            {content.updates.map((updateData) => (
+                                <span className={styles.update}>
+                                    {updateData.update}
+                                    <span className={styles.date}>
+                                        {updateData.date}
+                                    </span>
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div className={styles.item}>
-              {content.status}
-            </div>
-            <div className={styles.item}>
-              {/* TODO: add updates */}
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  )
+        </>
+    )
 }
